6a94a768713bfc8f27e6f024f2b31d7a6d05067a
hansel <58466533+JHXs@users.noreply.github.com>
2026-06-05 19:25:55 +0800

    支持参数指定前缀+出站网卡

--- edit-proxies.js
+++ edit-proxies.js
@@ -1,2 +1,13 @@
-$server.name = '[ikuuu]' + $server.name // 节点前面添加前缀
-$server['interface-name'] = 'wlp11s0' //指定节点的出口网卡
+const prefix = $arguments.prefix || $arguments.name || 'ikuuu'
+const interfaceName = $arguments.interfaceName || $arguments.interface || 'wlp11s0'
+
+if (prefix) {
+  const tag = `[${prefix}]`
+  if (!$server.name.startsWith(tag)) {
+    $server.name = tag + $server.name // 节点前面添加前缀
+  }
+}
+
+if (interfaceName) {
+  $server['interface-name'] = interfaceName // 指定节点的出口网卡
+}
